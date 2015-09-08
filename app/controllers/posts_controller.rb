9c62bd3b1fd79946b8116b8fca9de738a3ec31fd
dante411x <dante411x@gmail.com>
2015-09-08 08:08:57 -0400

    Add Validation to Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,18 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
-    @post.save
 
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+
   end
 
   def show
@@ -18,7 +23,9 @@ class PostsController < ApplicationController
   end
 
   private
-    def post_params
-      params.require(:post).permit(:title, :body)
-    end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
